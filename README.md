25a7a43fca5628cfc91d916d0659d91a77931e01
asaldana1108 <asaldana1108@gmail.com>
2020-08-02 22:25:41 -0500

    get routes and post routes added and working

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description 
+This app allows users to write and save notes. Express js was used to write and retrieve data from a JSON file on the backend. 
+
+## Badges
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents (Optional)
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+Clone repository, npm install, and install express. Use npm start to run the app. 
+
+## License
+MIT
+---
+
+## Questions
+For any additional questions, here is my contact information. 
+* [GitHub] (github.com/asaldana1108)
+* [Email] (asaldana1108@gmail.com)
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const {notes} = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+
 function createNewNote(body, notesArray) {
     const note = body; 
     notesArray.push(note);
@@ -36,6 +42,13 @@ app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, notes);
     res.json(newNote);
 });
+
+// app.delete('/api/notes/:id', (req, res) => {
+//     let id = parseInt(req.params.id);
+
+//     delete notes[id];
+//     res.send('Got a Delete request at...')
+// })
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
